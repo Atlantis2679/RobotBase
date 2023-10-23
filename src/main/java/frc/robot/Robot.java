d71de25e214712ae68791c130b5d0285a7aa5bfe
Roi Bar <roi3bar@gmail.com>
2023-10-23 14:40:15 +0300

    remove unnecessary import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.function.BooleanSupplier;
 
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LogTable;
